7aa2dfc80e2a0fc5059cd989009533bcf52fa359
Sid <135591304+sdknn@users.noreply.github.com>
2026-02-27 17:03:35 +0700

    Adding and updating files

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -6,8 +6,21 @@ namespace Framework;
 
 class App 
 {
+
+private Router $router;
+
+public function __construct()
+{
+  $this->router = new Router();
+}
+
   public function run() 
   {
     echo "Application is running";
+  }
+
+  public function add(string $path) 
+  {
+    $this->router->add($path);
   }
 }
---
+++ src/Framework/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class Router
+{
+  private array $routes = [];
+
+  public function add(string $path) 
+  {
+    $this->routes[] = [
+      'path' => $path
+    ];
+  }
+}
