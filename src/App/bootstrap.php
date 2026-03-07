a536ed04dfe9e6dfed0f0398035c8e6f948c4503
Sid <135591304+sdknn@users.noreply.github.com>
2026-03-07 17:33:28 +0700

    Add and update files

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
-require __DIR__ ."/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
--- src/App/views/partials/_footer.php
+++ src/App/views/partials/_footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="container mx-auto my-12 text-center">
-  &copy; PHPiggy
+  &copy; Celengan
 </footer>
 <!-- End Footer -->
 </body>
